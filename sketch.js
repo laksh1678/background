70e2e11ffda8463a30fedb4eb07fa40d5171d6a2
laksh1678 <88532434+laksh1678@users.noreply.github.com>
2021-08-19 13:15:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+  
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("purple");
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("yellow");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("orange");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("blue");
+  }
+
+  drawSprites();
+}
+
+
+
+
